6e0fd000cb90023ac512aad93ff9f43cf1acda3f
Bilel Dhraief <bdhraie@epsa.upv.es>
2026-03-03 19:34:17 +0100

    Github funcional

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
 import Home from './pages/home';
 import './styles/base.css'; // Asegúrate de que aquí está todo el CSS que creamos
 import Proyectos from './pages/proyectos';
